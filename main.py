9f4211e56a33ba79f7b834ae843cda2dac3b85e5
Ina Karapetyan <ina_karapetyan@bk.ru>
2024-12-13 16:12:16 +0400

    dates given as arguments

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
 import sys
-import os
-
-# SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-# sys.path.append(os.path.dirname(SCRIPT_DIR))
+import argparse
 from pipeline_dimensional_data.flow import DimensionalDataFlow
+from loggings import logger
+
+
+def get_args():
+    """
+    Parses command-line arguments for start and end dates.
+    """
+    parser = argparse.ArgumentParser(description="Run the dimensional data pipeline.")
+    parser.add_argument("--start_date", required=True, help="The start date (YYYY-MM-DD).")
+    parser.add_argument("--end_date", required=True, help="The end date (YYYY-MM-DD).")
+    return parser.parse_args()
+
 
 def main():
-    """
+    """clear
     Main function to execute the dimensional data pipeline.
     """
-    # Define start and end dates for the data flow execution
-    start_date = "2023-01-01"
-    end_date = "2024-12-31"
+    try:
 
-    # Create an instance of DimensionalDataFlow
-    data_flow = DimensionalDataFlow()
+        # Parse arguments
+        args = get_args()
+        start_date = args.start_date
+        end_date = args.end_date
 
-    # Execute the ETL pipeline
-    pipeline_status = data_flow.exec(start_date, end_date)
+        logger.info(f"Starting pipeline execution from {start_date} to {end_date}.")
 
-    # Print the final execution status
-    print("\nExecution completed!")
-    print("Final Pipeline Status:", pipeline_status)
+        # Create an instance of DimensionalDataFlow
+        data_flow = DimensionalDataFlow()
+
+        # Execute the ETL pipeline
+        pipeline_status = data_flow.exec(start_date, end_date)
+        if not isinstance(pipeline_status, dict):
+            raise TypeError(f"Expected a dictionary, got {type(pipeline_status)}")
+
+        logger.info("Execution completed!")
+        logger.info(f"Final Pipeline Status: {pipeline_status}")
+
+    except Exception as e:
+        logger.error(f"Pipeline execution failed: {e}", exc_info=True)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
